b3d52a528478dd25602088f8f6f14e099c26397b
chenyongwei <chenyongwei@syswin.com>
2018-07-06 10:42:38 +0800

    集成pod

---
+++ CFPicGame/Pods/Target Support Files/Pods-CFPicGame/Pods-CFPicGame-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CFPicGame : NSObject
+@end
+@implementation PodsDummy_Pods_CFPicGame
+@end
---
+++ CFPicGame/Pods/Target Support Files/Pods-CFPicGameUITests/Pods-CFPicGameUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CFPicGameUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CFPicGameUITests
+@end
